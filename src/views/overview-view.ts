9ba138d4a642cf488cc981991ec8df723cf627f7
elias-shalom <elias-shalom@hotmail.com>
2025-05-14 17:16:48 -0600

    Adding missing files overview-view.ts and overview-view.hbs

---
+++ src/views/overview-view.ts
@@ -0,0 +1,40 @@
+import { WorkspaceLeaf } from 'obsidian';
+import { BaseView } from '../views/base-view'; 
+import { TaskManager } from '../core/task-manager';
+import { ITask } from '../types/interfaces';
+import { I18n } from '../core/i18n';
+
+
+export const OVERVIEW_VIEW_TYPE = 'overview-view';
+
+export class OverviewView extends BaseView {
+  private tasks: ITask[] = []; // Lista de tareas
+
+
+  constructor(leaf: WorkspaceLeaf, private plugin: any, private i18n: I18n, private taskManager: TaskManager) {
+    super(leaf);
+    this.i18n = i18n;
+  }
+
+  getViewType(): string {
+    return OVERVIEW_VIEW_TYPE;
+  }
+
+  getDisplayText(): string {
+    return this.i18n.t("overview_view_title"); // Título de la vista principal
+  }
+
+  getIcon(): string {
+    return 'calendar-check';
+  }
+
+  async onOpen(): Promise<void> {
+    this.tasks = await this.getAllTasks(this.taskManager);
+    console.log("Tareas obtenidas:", this.tasks); // Debugging line
+    await this.render(OVERVIEW_VIEW_TYPE, { tasks: this.tasks }, this.i18n, this.plugin, this.leaf);
+  }
+
+  async onClose(): Promise<void> {
+    // Limpia recursos si es necesario
+  }
+}
